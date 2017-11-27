9874aeff1e2c7f4854b6bf8665930d568b398b70
Daniel Klimaj <xklima22@stud.fit.vutbr.cz>
2017-11-27 14:04:46 +0100

    [ADD] MqttService

---
+++ PDI/MqttService/MqttService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttService
+{
+    public class MqttService
+    {
+        public MqttService()
+        {
+            Console.WriteLine("Hello MQTT service");
+        }
+    }
+}
---
+++ PDI/TestCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var svc = new MqttService.MqttService();
+        }
+    }
+}
