6242b0108d20ff4d5146e1e44fe42bed1dc5276b
Daniel Klimaj <xklima22@stud.fit.vutbr.cz>
2017-11-28 14:27:16 +0100

    [ADD] Disfunctional DB

---
+++ PDI/ConsoleTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using MqttService.Models;
+using MqttService.Persistence;
+using MqttService.Persistence.Sql;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var svc = new MqttService.MqttService();
+            var mc = new Microcontroller("aaaa", false, 64.2);
+            var ps = new PowerStrip("bbbb");
+
+            Console.WriteLine("MC: {0}, {1}, {2}", mc.DeviceId, mc.Powered, mc.Temperature);
+            Console.WriteLine("PS: {0}, {1}", ps.DeviceId, ps.Powered);
+
+            var db = new MqttServiceDbContext("Data Source=./TestDB.sqlite");
+            var mcr = new MicrocontrollerSqlRepository(db);
+            var psr = new PowerStripSqlRepository(db);
+
+            Console.WriteLine("Trying to load data");
+            List<Microcontroller> mcrs = mcr.AllMicrocontrollers().ToList();
+            List<PowerStrip> pwss = psr.AllPowerStrips().ToList();
+
+            Console.WriteLine("Microcontrollers: {}", mcrs.Count());
+            Console.WriteLine("Power Strips: {}", pwss.Count());
+        }
+    }
+}
--- PDI/MqttService/Persistence/MqttServiceDbContext.cs
+++ PDI/MqttService/Persistence/MqttServiceDbContext.cs
@@ -1,10 +1,18 @@
 ﻿using System.Data.Entity;
 using MqttService.Persistence.Entity;
+using System.Data.SQLite;
 
 namespace MqttService.Persistence
 {
     public class MqttServiceDbContext : DbContext
     {
+        public MqttServiceDbContext() : base("name=Database")
+        {
+            Database.SetInitializer<MqttServiceDbContext>(
+                new DropCreateDatabaseIfModelChanges<MqttServiceDbContext>()
+            );
+        }
+
         public MqttServiceDbContext(string connectionString) : base(connectionString)
         {
             Database.SetInitializer<MqttServiceDbContext>(
